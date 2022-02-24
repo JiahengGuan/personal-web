535e58731876efcde9bc749eac72d7fe58b15dc7
Jiaheng Guan <gjh1547485384@gmail.com>
2022-02-24 13:36:46 -0500

    pushed

--- src/component/Home.js
+++ src/component/Home.js
@@ -19,38 +19,12 @@ const { Panel } = Collapse;
 
 
 function Home(props) {
-    const menu = (
-        <Menu>
-          <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-              1st menu item
-            </a>
-          </Menu.Item>
-          <Menu.Item icon={<DownOutlined />} disabled>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-              2nd menu item (disabled)
-            </a>
-          </Menu.Item>
-          <Menu.Item disabled>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-              3rd menu item (disabled)
-            </a>
-          </Menu.Item>
-          <Menu.Item danger>a danger item</Menu.Item>
-        </Menu>
-      );
     return (
             <div id = "homepage">
                 <Layout className="layout">
                     <Header>
                     </Header>
                     <Content className = "content" >
-                    <Dropdown className = "dropdown" 
-                    overlay={menu}>
-      <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-      Hover me <DownOutlined />
-    </a>
-  </Dropdown>
                         <div class = "blocks" >
                             <div class="odd">
                                 <Title style={{ textAlign: 'center' }} >Fenghsi Yu</Title>
