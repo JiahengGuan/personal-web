fbf63ab020cc33cfe786bcce7f4c07e47eeb4c77
Jiaheng Guan <gjh1547485384@gmail.com>
2022-02-23 23:29:08 -0500

    pushed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div>
           <div className="container">
               <Switch>
-                  <Route exact path="/my-app" render={() => (<Home/>)} />
+                  <Route exact path="/personal-web" render={() => (<Home/>)} />
               </Switch>
           </div>
     </div>
